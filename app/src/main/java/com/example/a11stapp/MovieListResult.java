da75b94fdf8f0cfff0d9ce6673fec94db2ed767b
serin0157 <serin0157@naver.com>
2022-05-24 19:04:22 +0900

    12주차 강의 내용 추가

---
+++ app/src/main/java/com/example/a11stapp/MovieListResult.java
@@ -0,0 +1,10 @@
+package com.example.a11stapp;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxOfficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
